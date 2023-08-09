af24744940365781b7dda3bf7882abde39592fd6
MrParadocs <harshitsh1226@gmail.com>
2023-08-09 17:31:18 +0530

    update css and addded show result functionality

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 "use strict";
 import quizQuestions from './data.js';
-let highscore = document.getElementsByClassName("highscore");
 let highscoreNumber = document.getElementById("highscore");
 let timer = document.getElementsByClassName("timer");
 let timeLeft = document.getElementById("timeLeft");
@@ -14,11 +13,9 @@ let submitBtn = document.getElementById("submit");
 let skipBtn = document.getElementById("skip-button");
 let nextBtn = document.getElementById("next");
 let result = document.getElementById("result")
-
-const option1Input = document.getElementById('option1');
-const option2Input = document.getElementById('option2');
-const option3Input = document.getElementById('option3');
-const option4Input = document.getElementById('option4');
+let resultDiv = document.getElementById("result-div");
+let userScore = document.getElementById("user-score");
+let resultBtn = document.getElementById("resultBtn");
 
 const option1Label = document.querySelector('label[for="option1"]');
 const option2Label = document.querySelector('label[for="option2"]');
@@ -32,7 +29,7 @@ let questionIndex = 0;
 let score = 0
 //staring the quiz and rendering the first question
 function startQuiz() {
-    
+
 }
 startBtn.addEventListener('click', (e) => {
     intro.classList.add("hidden");
@@ -61,13 +58,13 @@ submitBtn.addEventListener('click', (e) => {
     if (selectedOption !== null && !isNaN(selectedOption.value)) {
         let selectedValue = parseInt(selectedOption.value);
         let correctAnswer = quizQuestions[questionIndex].answer;
-        clearInterval(interval)
+        clearInterval(interval);
         skipBtn.style.display = "none";
         if (correctAnswer === selectedValue) {
-            answer.style.display = "block"
+            answer.style.display = "block";
             answer.innerText = "Correct";
             score += 2;
-            highscoreCalculation(score)
+            highscoreCalculation(score);
         } else {
             score--;
             highscoreCalculation(score);
@@ -79,12 +76,15 @@ submitBtn.addEventListener('click', (e) => {
 
         if (questionIndex < quizQuestions.length - 1) {
             nextBtn.style.display = "block";
+        } else {
+            // Show the "See Your Score" button when submitting the last question
+            nextBtn.style.display = "none";
+            resultBtn.style.display = "block";
         }
     } else {
-        alert("select a value");
+        alert("Select an answer.");
         console.log("Invalid selectedValue:", selectedOption);
     }
-
 });
 
 //Skipping The answer
@@ -99,13 +99,15 @@ skipBtn.addEventListener('click', () => {
     if (questionIndex < quizQuestions.length) {
         answer.style.display = "none";
         showQuestion(questionIndex);
-        updateTimerDisplay(counter, "black");
+        updateTimerDisplay(counter, "#D3D3D3");
         answer.innerText = "";
         submitBtn.style.display = "block";
         nextBtn.style.display = 'none';
     } else {
-        result.style.display = "block"
-        result.innerText = "Quiz completed!";
+        timeLeft.innerText = "__"
+        result.style.display = "block";
+        userScore.innerText = score;
+        resultBtn.style.display = "block"; // Show the "See Your Score" button when skipping the last question
     }
 });
 
@@ -164,3 +166,10 @@ function updateTimerDisplay(counter, color) {
     timeLeft.innerText = counter + "s";
     timer[0].style.color = color;
 }
+
+resultBtn.addEventListener('click', () => {
+    timeLeft.innerText = "__"
+    quizWrapper.style.display = "none";
+    resultDiv.style.display = "block";
+    userScore.innerText = score;
+});
